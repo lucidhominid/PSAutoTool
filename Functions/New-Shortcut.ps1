bebff44e08dff7d6ddf25895e824da8e6b7a9dc3
lucidhominid <lucidhominid@gmail.com>
2021-03-22 06:09:50 -0700

    Improved Send-Key and Get-Cursor

--- Functions/New-Shortcut.ps1
+++ Functions/New-Shortcut.ps1
@@ -3,18 +3,29 @@ param (
     [Parameter()]
     [String]
     $Path = (Get-Location),
+
     [Parameter()]
     [String]
     $TargetPath,
+
     [Parameter()]
     $ArgumentList,
+
     [Parameter()]
-    [ValidateSet('Normal','Minimized','Maximized','Hidden')]
-    [String]$WindowStyle = 'Normal',
+    [ValidateSet(
+        'Normal',
+        'Minimized',
+        'Maximized',
+        'Hidden'
+    )][String]
+    $WindowStyle = 'Normal',
+
     [Parameter()]
     $Hotkey,
+
     [Parameter()]
     $RelativePath,
+
     [Parameter()]
     [String]
     $WorkingDirectory
@@ -28,18 +39,15 @@ $Shortcut.Hotkey           = $Hotkey
 $Shortcut.RelativePath     = $RelativePath
 $Shortcut.TargetPath       = $TargetPath
 $Shortcut.WorkingDirectory = $WorkingDirectory
-$Shortcut.WindowStyle      = Switch($WindowStyle)
-{
+$Shortcut.WindowStyle      = Switch($WindowStyle){
     'Minimized' {0;Break}
     'Maximized' {2;Break}
     'Hidden'    {3;Break}
     Default     {1}
 }
-if($Save)
-{
+if($Save){
     $Shortcut.Save()
 }
-else 
-{
+else{
     $Shortcut
 }
