f7e168b450570134a552601ecb79ee38925f93e6
lucidhominid <lucidhominid@gmail.com>
2021-10-26 18:10:50 -0700

    Sync

--- Functions/ConvertTo-MD5Hash.ps1
+++ Functions/ConvertTo-MD5Hash.ps1
@@ -24,3 +24,16 @@ process
         )
     ).tolower() -replace '-'
 }
+
+
+
+"C:\Program Files (x86)\Advanced IP Scanner\advanced_ip_scanner.exe" |
+    Get-Item|
+    Foreach-Object{
+        $Shortcut = $WScriptShell.CreateShortcut(
+           
+        )
+    
+    }
+
+C:\Windows\powershell.exe -notexit {$WScriptShell = (New-Object -ComObject WScript.Shell).CreateShortcut( ($Args[0].split('\') -replace '\.[^\.]+$','.lnk')) |%{$_.TargetPath=$Args[0];$_.Save()}} "%1"
